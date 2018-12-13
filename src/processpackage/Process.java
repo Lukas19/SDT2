5af7516daceb2df75b78436454e542d40ed05d70
ivalenzu <menthos123@gmail.com>
2018-12-13 04:44:46 -0300

    Implementacion prematura de RMI, falta revisar los argumentos

--- src/processpackage/Process.java
+++ src/processpackage/Process.java
@@ -1,64 +1,115 @@
 package processpackage;
 
 import java.rmi.*;
-
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 public class Process implements ProcessInterface{
 
-    static ProcessInterface stub2;
-    public String id;
     public String[] neighbors;
     public boolean isInitiator;
-    public boolean isCommited = false;
-    public int n = 0;
+    public int received = 0;
+    public String id;
 
-    public Process() throws RemoteException  {
-
+    public Process(String[] neighbors, boolean isInitiator, String id) throws RemoteException  {
+        this.neighbors = neighbors;
+        this.isInitiator = isInitiator;
+        this.id = id;
     }
 
-
     public static void main(String[] args) {
-
         //input
         String id = args[0];
         String[] neighbors = args[1].split(",");
         boolean isInitiator = Boolean.parseBoolean(args[2]);
-        String route = args[3];
-        String ip = args[4];
+        if(isInitiator) {
+            String route = args[3];
+            String ip = args[4];
+        }
 
         try {
-            Process obj = new Process();
+            Process obj = new Process(neighbors, isInitiator, id);
             ProcessInterface stub2 = (ProcessInterface) UnicastRemoteObject.exportObject(obj, 0);
-            System.out.println(stub2);
             // Bind the remote object's stub in the registry
-            LocateRegistry.createRegistry(2000);
-            Registry registry = LocateRegistry.getRegistry(2000);
-            registry.bind(id, stub2);
-
-            System.err.println("Node" + id + " is  ready");
-            stub2.send(neighbors, id);
+            Registry registry = LocateRegistry.getRegistry();
+            try {
+                registry.bind(id, stub2);
+            } catch (AlreadyBoundException e){
+                System.out.println("Node already bound to registry \n");
+            }
+            System.err.println("Node " + id + " is  ready");
+            if(isInitiator){
+                System.out.println("Sending something");
+                stub2.send(id);
+            }
         } catch (RemoteException e) {
             System.out.println("Couldnt bind node to registry\n");
-            e.printStackTrace();
-        } catch (AlreadyBoundException e) {
-            System.out.println("Node already bound to registry \n");
             e.printStackTrace();
         }
 
     }
 
     @Override
-    public void send (String[] vecinos, String origen, boolean iniciador) throws RemoteException {
-        if (iniciador) {
-            n = 0;
+    public void send (String idOrigin) throws RemoteException {
+        if (isInitiator) {
+            for(String ids : neighbors){
+                try{
+                    Registry reg = LocateRegistry.getRegistry();
+                    ProcessInterface stub = (ProcessInterface) reg.lookup(ids);
+                    stub.send(ids);
+
+                    while(received < neighbors.length){
+                        continue;
+                    }
+                    System.out.println(idOrigin + ": I'm ready with responses");
+                } catch (NotBoundException e) {
+                    e.printStackTrace();
+                }
+            }
         }
         else {
-            System.out.println("Explorer recibido de proceso" + origen);
+            System.out.println("Explorer recibido de proceso " + idOrigin);
+            String parent = idOrigin;
+            received += 1;
+            for(String ids : neighbors){
+                if(!ids.equals(parent)){
+                    try{
+                        Registry reg = LocateRegistry.getRegistry();
+                        ProcessInterface stub = (ProcessInterface) reg.lookup(ids);
+                        stub.send(ids);
+
+                        while(received < neighbors.length){
+                            continue;
+                        }
+
+                        stub = (ProcessInterface) reg.lookup(parent);
+                        stub.sendOk(idOrigin, parent);
+                    } catch (NotBoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
-        //Investigar como  almacenar la data del proceso para poder empezar a hacer exploración.
+    }
+
+    @Override
+    public void sendOk (String from, String to) throws RemoteException{
+        if(!id.equals(to)) {
+            try {
+                Registry reg = LocateRegistry.getRegistry();
+                ProcessInterface stub = (ProcessInterface) reg.lookup(to);
+                System.out.println("Sending OK to :" + to);
+                stub.sendOk(from, to);
+
+            } catch (NotBoundException e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.out.println(from + " Replied with Ok...");
+            received += 1;
+        }
     }
 
 }
--- src/processpackage/ProcessInterface.java
+++ src/processpackage/ProcessInterface.java
@@ -4,6 +4,8 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 
 public interface ProcessInterface extends Remote {
-    public void send(String[] vecinos, String origen) throws RemoteException;
+    public void send(String origen) throws RemoteException;
+
+    public void sendOk(String from, String to) throws RemoteException;
 
 }
